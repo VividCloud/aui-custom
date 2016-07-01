d6c5ab50395c303c04e41f30e821907b1fd0a28c
laosb <laosb@lasteam.net>
2016-07-01 12:15:59 +0800

    Quick fix of server failures.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'vvd:aui-custom',
-    version: '0.1.0',
+    version: '0.1.1',
     summary: 'Customize accounts-ui.',
     git: 'https://github.com/VividCloud/aui-custom',
     documentation: 'README.md'
@@ -8,8 +8,8 @@ Package.describe({
 
 Package.onUse(function(api) {
     api.versionsFrom('1.3.4.1');
-    api.use('ecmascript');
-    api.use('templating');
-    api.use('accounts-ui-unstyled');
-    api.mainModule('aui-custom.js');
+    api.use('ecmascript', 'client');
+    api.use('templating', 'client');
+    api.use('accounts-ui-unstyled', 'client');
+    api.mainModule('aui-custom.js', 'client');
 });
